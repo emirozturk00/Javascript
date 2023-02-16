d0c9a3c90427e759a7fd9ae2f2de16eb5e05afed
Emir Öztürk <111645804+emirozturk00@users.noreply.github.com>
2023-02-16 08:49:09 +0300

    ...

--- Variables.js
+++ Variables.js
@@ -218,3 +218,27 @@ console.log("number4: ", number4, typeof(number4) )
 let number5 = 55
 number5 = number5.toString()
 console.log(number5, typeof(number5))
+
+// ********** Template Literals Kullanimi ********** 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+let username = "hakan"
+const DOMAIN = "kodluyoruz.org"
+
+let email = username + "@" + DOMAIN
+
+// console.log("Merhaba", username, "sitemize hosgeldin", "mail adresin:", email)
+
+let info = `
+Merhaba ${username} sitemize hosgeldin.. 
+mail adresin: ${email}
+
+mail adresinin uzunlugu: ${email.length}
+borcunuz: ${(2 + 3) * 10} TL
+gunun saat bilgisi : ${new Date().getHours()}
+
+kisa isminiz: ${username[0]}.
+`
+
+console.log(info)
+
